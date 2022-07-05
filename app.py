2bd89abb402264d09568e44c6b871863e4d28b7d
dartbill <billieldartnell@hotmail.com>
2022-07-05 14:49:52 +0100

    comment out postgres

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# db = SQLAlchemy(app)
 
-class Item(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(80), unique=True, nullable=False)
-  content = db.Column(db.String(120), unique=True, nullable=False)
+# class Item(db.Model):
+#   id = db.Column(db.Integer, primary_key=True)
+#   title = db.Column(db.String(80), unique=True, nullable=False)
+#   content = db.Column(db.String(120), unique=True, nullable=False)
 
-  def __init__(self, title, content):
-    self.title = title
-    self.content = content
+#   def __init__(self, title, content):
+#     self.title = title
+#     self.content = content
 
-db.create_all()
+# db.create_all()
 
 @app.route('/items/<id>', methods=['GET'])
 def get_item(id):
