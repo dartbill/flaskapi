cffc707bf55c2655b178557679301d54765165f6
dartbill <billieldartnell@hotmail.com>
2022-07-05 14:56:56 +0100

    try to test sql

--- app.py
+++ app.py
@@ -8,25 +8,25 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
 
-# class Item(db.Model):
-#   id = db.Column(db.Integer, primary_key=True)
-#   title = db.Column(db.String(80), unique=True, nullable=False)
-#   content = db.Column(db.String(120), unique=True, nullable=False)
+class Item(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(80), unique=True, nullable=False)
+  content = db.Column(db.String(120), unique=True, nullable=False)
 
-#   def __init__(self, title, content):
-#     self.title = title
-#     self.content = content
+  def __init__(self, title, content):
+    self.title = title
+    self.content = content
 
-# db.create_all()
+db.create_all()
 
-# @app.route('/items/<id>', methods=['GET'])
-# def get_item(id):
-#   item = Item.query.get(id)
-#   del item.__dict__['_sa_instance_state']
-#   return jsonify(item.__dict__)
+@app.route('/items/<id>', methods=['GET'])
+def get_item(id):
+  item = Item.query.get(id)
+  del item.__dict__['_sa_instance_state']
+  return jsonify(item.__dict__)
 
 @app.route('/')
 def home():
